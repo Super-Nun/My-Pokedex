2a09deca5f83177e95d26a4ac25f2e613574f123
Super-Nun <kanokphon.fu@gmail.com>
2025-02-18 15:03:28 +0700

    update responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,7 @@ export default function Home() {
   };
 
   return (
-    <div className="container mt-10 mb-5 py-10 px-15 rounded-4xl max-w-7xl h-190 justify-self-center bg-gray-900 text-white">
+    <div className="container mt-15 mb-5 ms-full me-full py-10 px-15 rounded-4xl max-w-7xl h-190 justify-self-center bg-gray-900 text-white">
       <div className="container max-w-6xl h-170 font-bold bg-white flex flex-col justify-center items-center">
         {/* title */}
         <div className="container text-gray-800 text-5xl flex flex-col text-center p-3">
@@ -55,7 +55,7 @@ export default function Home() {
         </div>
 
         {/* details */}
-        <div className="container inset-shadow-sm inset-shadow-gray-300 text-gray-400 bg-gray-200 text-4xl flex flex-col items-center p-3 h-full overflow-y-auto">
+        <div className="container inset-shadow-sm inset-shadow-gray-300 text-gray-400 bg-gray-200 text-4xl   p-3 h-full w-full overflow-scroll">
           {favorites.length > 0 ? (
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
               {favorites.map((p) => (
@@ -72,7 +72,7 @@ export default function Home() {
               ))}
             </div>
           ) : (
-            <p className="text-gray-300 flex justify-center items-center h-full">Select a Pokémon to add to favorites.</p>
+            <p className="text-gray-300 flex flex-col items-center justify-center text-lg lg:text-5xl h-full">Select a Pokémon to add to favorites.</p>
           )}
         </div>
 
@@ -87,126 +87,96 @@ export default function Home() {
         </div>
 
         {showSearch && (
-          <div className="absolute overflow-scroll top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4xl h-150 lg:w-5xl text-black p-5 rounded-md shadow-2xl aspect-video bg-gray-50 border-l-inherit">
-            <div className="flex justify-between items-center mb-4">
-              <input
-                type="text"
-                placeholder="Search Pokémon..."
-                className="p-2 w-full border rounded"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <button onClick={() => setShowSearch(false)} className="text-red-600 text-4xl ml-4">
-                &times;
-              </button>
-            </div>
-            {search && filteredPokemon.length > 0 ? (
-              <div className="grid grid-rows justify-center gap-2 ">
-                {filteredPokemon.map((p) => (
-                  <div
-                    key={p.id}
-                    className="container bg-blue-100 inset-shadow- inset-shadow-gray-600 w-4xl pt-5 pb-5 rounded-lg text-center flex flex-row cursor-pointer hover:scale-110 transition-transform"
-                    onClick={() => handleAddFavorite(p)}
-                  >
-                    <div className="ps-10 pe-10 space-y-1 ">
-                      <h2 className="text-xl font-bold capitalize ">{p?.name}</h2>
-                      <img src={p.imageUrlHiRes} alt={p.name} className="mx-auto h-40" />
-                      <p className="text-lg text-gray-500">#{p?.nationalPokedexNumber || '-'}</p>
-                    </div>
+            <div className="absolute overflow-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-xs h-130 sm:max-w-sm md:max-w-lg lg:max-w-5xl lg:h-130 text-black p-5 rounded-2xl shadow-2xl aspect-video bg-gray-100 border-l-inherit">
+                <div className="flex justify-between items-center mb-4">
+                <input
+                    type="text"
+                    placeholder="Search Pokémon..."
+                    className="p-2 w-full border rounded"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <button onClick={() => setShowSearch(false)} className="text-red-600 text-4xl ml-4">
+                    &times;
+                </button>
+                </div>
 
-                    {/* Display Card Details Based on Supertype */}
-                    {p.supertype === 'Pokémon' ? (
-                      <div className="ps-10 pe-10 w-full p-2">
-                        <div className="flex justify-between items-center mb-5">
-                          <span className="bg-green-600 text-white px-4 py-2 rounded-lg text-lg font-semibold shadow-md">
-                            HP: {p?.hp || '-'}
-                          </span>
-                          <span className="bg-blue-500 text-white px-4 py-2 rounded-lg text-lg font-semibold shadow-md">
-                            Type: {p?.type || 'Unknown'}
-                          </span>
+                {search && filteredPokemon.length > 0 ? (
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
+                    {filteredPokemon.map((p) => (
+                    <div
+                        key={p.id}
+                        className="bg-blue-100 shadow-md p-4 rounded-lg text-center flex flex-col md:flex-row cursor-pointer hover:scale-105 transition-transform items-center "
+                        onClick={() => handleAddFavorite(p)}
+                    >
+                        {/* 📌 ส่วนแสดงภาพและชื่อการ์ด */}
+                        <div className="w-full md:w-1/3 flex flex-col items-center">
+                        <h2 className="text-lg font-bold capitalize">{p?.name}</h2>
+                        <img src={p.imageUrlHiRes} alt={p.name} className="mx-auto h-32 sm:h-40" />
+                        <p className="text-gray-500 text-lg">#{p?.nationalPokedexNumber || '-'}</p>
                         </div>
 
-                        {/* Ability */}
-                        {p.ability && (
-                          <div className="pb-4 flex flex-col items-start">
-                            <h3 className="text-md font-semibold text-gray-700 mb-1">✨ Ability: {p.ability.name}</h3>
-                            <p className="text-gray-600 text-xs font-mono text-justify">{p.ability.text}</p>
-                          </div>
-                        )}
-
-                        {/* Other Details */}
-                        <div className="grid grid-cols-2 gap-4">
-                          {p.retreatCost && (
-                            <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-                              <h3 className="text-md font-semibold text-gray-700 mb-1">🏃 Retreat Cost</h3>
-                              <p className="text-gray-600 text-sm">{p.retreatCost.toString() || '-'}</p>
+                        {/* 📌 เงื่อนไขของการ์ดแต่ละประเภท */}
+                        <div className="w-full md:w-2/3 hidden md:block">
+                        {p.supertype === 'Pokémon' && (
+                            <div className="mt-2">
+                            <div className="flex justify-between text-sm">
+                                <span className="bg-green-600 text-white px-2 py-1 rounded-lg">HP: {p?.hp || '-'}</span>
+                                <span className="bg-blue-500 text-white px-2 py-1 rounded-lg">Type: {p?.type || 'Unknown'}</span>
                             </div>
-                          )}
 
-                          {p.weaknesses && p.weaknesses.length > 0 && (
-                            <div className="bg-red-100 p-4 rounded-lg shadow-md">
-                              <h3 className="text-md font-semibold text-red-500 mb-1">❌ Weaknesses</h3>
-                              <ul className="list-disc pl-5 text-gray-700 text-sm">
-                                {p.weaknesses.map((weakness, index) => (
-                                  <li key={index}>{weakness.type} ({weakness.value})</li>
-                                ))}
-                              </ul>
-                            </div>
-                          )}
-
-                          {p.resistances && p.resistances.length > 0 && (
-                            <div className="bg-green-100 p-4 rounded-lg shadow-md">
-                              <h3 className="text-md font-semibold text-green-500 mb-1">🛡 Resistances:</h3>
-                              <ul className="list-disc pl-5 text-gray-700 text-sm">
-                                {p.resistances.map((resistance, index) => (
-                                  <li key={index}>{resistance.type} ({resistance.value})</li>
-                                ))}
-                              </ul>
-                            </div>
-                          )}
-                        </div>
-
-                        {/* Attacks */}
-                        {p.attacks && p.attacks.length > 0 && (
-                          <div className="mt-4">
-                            <h3 className="text-lg font-semibold text-gray-700">⚔️ Attacks</h3>
-                            <div className="md:grid-cols-2 gap-4 mt-2">
-                              {p.attacks.map((attack, index) => (
-                                <div key={index} className="p-4 rounded-lg inset-shadow-sm">
-                                  <div className="flex flex-row space-x-5">
-                                    <h4 className="text-md font-semibold">{attack.name}</h4>
-                                    <span className="block text-gray-800 font-bold">💥 Damage: {attack.damage || 'N/A'}</span>
-                                  </div>
-                                  <p className="text-gray-600 text-xs font-mono text-justify">{attack.text}</p>
+                            {p.ability && (
+                                <div className="mt-2 text-xs">
+                                <h3 className="font-semibold">✨ Ability: {p.ability.name}</h3>
+                                <p className="text-gray-600 text-justify font-light">{p.ability.text}</p>
                                 </div>
-                              ))}
+                            )}
+
+                            {p.attacks && p.attacks.length > 0 && (
+                                <div className="mt-4">
+                                <h3 className="text-lg font-semibold text-gray-700">⚔️ Attacks</h3>
+                                <ul className="list-disc pl-5 text-gray-600 text-sm">
+                                    {p.attacks.map((attack, index) => (
+                                    <p key={index}>
+                                        {attack.name}  <span className="font-bold ">{attack.damage || 'N/A'}</span>
+                                    </p>
+                                    ))}
+                                </ul>
+                                </div>
+                            )}
                             </div>
-                          </div>
                         )}
-                      </div>
-                    ) : p.supertype === 'Trainer' ? (
-                      <div className="ps-10 pe-10 text-gray-600 space-y-5 flex flex-col justify-center text-justify w-2xl">
-                        <h2 className="uppercase text-lg">Trainer Card</h2>
-                        <p className="font-mono">
-                          {Array.isArray(p.text) ? p.text.join(' ') : p.ability?.text || 'NaN'}
-                        </p>
-                      </div>
-                    ) : p.supertype === 'Energy' ? (
-                      <div className="ps-10 pe-10 text-gray-600 space-y-5 flex flex-col justify-center text-justify w-2xl">
-                        <h2 className="uppercase text-lg">Energy Card</h2>
-                        <p className="font-mono">
-                          {Array.isArray(p.rules) ? p.rules.join(' ') : p.text || 'NaN'}
-                        </p>
-                      </div>
-                    ) : null}
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <p className="text-gray-500 flex flex-row justify-center mt-25 text-center">No Pokémon found.</p>
+
+                        {/* 🔹 Trainer Card */}
+                        {p.supertype === 'Trainer' && (
+                            <div className="ps-4 pe-4 text-gray-600 flex flex-col justify-center text-justify">
+                            <h2 className="uppercase text-lg text-center font-bold text-gray-500">Trainer Card</h2>
+                            <p className="font-mono text-sm font-light">
+                                {Array.isArray(p.text) ? p.text.join(' ') : p.ability?.text || 'No information available'}
+                            </p>
+                            </div>
+                        )}
+
+                        {/* 🔹 Energy Card */}
+                        {p.supertype === 'Energy' && (
+                            <div className="ps-4 pe-4 text-gray-600 flex flex-col justify-center text-justify">
+                            <h2 className="uppercase text-lg text-center font-bold text-yellow-500">Energy Card</h2>
+                            <p className="font-mono text-sm font-light">
+                                {Array.isArray(p.rules) ? p.rules.join(' ') : p.text || 'No information available'}
+                            </p>
+                            </div>
+                        )}
+                        </div>
+                    </div>
+                    ))}
+                </div>
+                ) : (
+                <p className="text-gray-500 text-center mt-4">No Pokémon found.</p>
+                )}
+            </div>
             )}
-          </div>
-        )}
+
+
       </div>
     </div>
   );
